24ddb68435d719f22ed03d80fb6e29a19194bf5f
Емельянов Максим <masiko-17@mail.ru>
2024-11-07 22:50:20 +0500

    Создаст объект данных для описания общежитий в Slider

--- src/components/SliderSection/SliderSection.jsx
+++ src/components/SliderSection/SliderSection.jsx
@@ -2,38 +2,14 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import './SliderSection.css';
-
-const cards = [
-    {
-        id: 1,
-        imageUrl: '../../../public/dormitory1.jpg',
-        title: 'Card Title 1',
-        description: 'Description for Card 1'
-    },
-    {
-        id: 2,
-        imageUrl: '../../../public/dormitory2.jpg',
-        title: 'Card Title 2',
-        description: 'Description for Card 2'
-    },
-    {
-        id: 3,
-        imageUrl: '../../../public/dormitory3.jpg',
-        title: 'Card Title 3',
-        description: 'Description for Card 3'
-    },
-    {
-        id: 4,
-        imageUrl: '../../../public/dormitory4.jpg',
-        title: 'Card Title 4',
-        description: 'Description for Card 4'
-    }
-];
+import { YMaps, Map, Placemark, GeolocationControl, RouteButton, SearchControl, ZoomControl } from '@pbe/react-yandex-maps';
+import { cards } from '../../data/InfoDormitory';
 
 
 export default function SliderSection() {
     const settings = {
-        dots: true,
+        draggable: false,
+        dots: false,
         infinite: true,
         speed: 1200,
         slidesToShow: 1,
@@ -43,21 +19,46 @@ export default function SliderSection() {
         pauseOnHover: true,
 
         accessibility: true,
-        centerPadding: '30px'
+        centerMode: true,
     };
 
     return (
-        <section className='sliders container'>
+        <section className='sliders-container'>
             <h2>Slider</h2>
             <Slider {...settings}>
                 {cards.map(card => (
                     <section key={card.id} className='slider-container'>
-                        <div>
-                            <h3>{card.title}</h3>
-                            <p>{card.description}</p>
-                            <a href="#">ссылка</a>
+                        <div className="content">
+                            <div className="left-side">
+                                <h3>{card.title}</h3>
+                                <p>{card.description}</p>
+                                <a href="#">ссылка</a>
+                            </div>
+                            <div className="right-side">
+                                <YMaps query={{ apikey: '9f8f472c-08bd-4dbb-8ecb-aeb1629d15e7' }}>
+                                    <section className='map-slider-container'>
+                                        <Map state={{
+                                            center: card.position,
+                                            zoom: 15,
+                                        }} style={{ width: '500px', height: '500px', margin: '0 auto', borderRadius: '10px' }}>
+                                            <Placemark
+                                                geometry={card.position}
+                                                properties={{
+                                                    balloonContentBody: `
+                            <div class="mark-container" style='width: 200px'>
+                              <h3>${card.title}</h3>
+                              <img src="${card.imageUrl}" width='200'/>
+                              <p>${card.descriptionPlacemark}</p>
+                            </div>
+                              `,
+                                                }}
+                                                modules={['geoObject.addon.balloon']}
+                                            />
+                                        </Map>
+                                    </section>
+                                </YMaps>
+                            </div>
                         </div>
-                        <img src={card.imageUrl} alt={card.title} />
                     </section>
                 ))}
             </Slider>
